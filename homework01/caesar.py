56d847eb7c37cacefc2abc7f8de457fb5d5f3af5
dbalakin <55895317+dbalakin@users.noreply.github.com>
2019-10-12 13:23:36 +0300

    git commit -m "исправил код"

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,55 +1,50 @@
 def encrypt_caesar(plaintext: str) -> str:
     """
-        >>> encrypt_caesar("PYTHON")
-        'SBWKRQ'
-        >>> encrypt_caesar("python")
-        'sbwkrq'
-        >>> encrypt_caesar("Python3.6")
-        'Sbwkrq3.6'
-        >>> encrypt_caesar("")
-        ''
-        """
-    # PUT YOUR CODE HERE
+    >>> encrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> encrypt_vigenere("python", "a")
+    'python'
+    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
+    'LXFOPVEFRNHR'
+    """
     ciphertext = ""
-    for i in plaintext:
-        if 65 <= ord(i) <= 90:
-            if ord(i) + 3 > ord("Z"):
-                ciphertext += chr(ord(i) + 3 - 26)
+    for ch in plaintext:
+        if "A" <= ch <= "Z":
+            if ch + 3 > "Z":
+                ciphertext += chr(ch + 3 - 26)
             else:
-                ciphertext += chr(ord(i) + 3)
-        elif 97 <= ord(i) <= 122:
-            if ord(i) + 3 > ord("z"):
-                ciphertext += chr(ord(i) + 3 - 26)
+                ciphertext += chr(ch + 3)
+        elif "a" <= ch <= "z":
+            if ch + 3 > "z":
+                ciphertext += chr(ch + 3 - 26)
             else:
-                ciphertext += chr(ord(i) + 3)
+                ciphertext += chr(ch + 3)
         else:
             ciphertext += i
     return ciphertext
 
+
 def decrypt_caesar(ciphertext: str) -> str:
     """
-        >>> decrypt_caesar("SBWKRQ")
-        'PYTHON'
-        >>> decrypt_caesar("sbwkrq")
-        'python'
-        >>> decrypt_caesar("Sbwkrq3.6")
-        'Python3.6'
-        >>> decrypt_caesar("")
-        ''
-        """
-    # PUT YOUR CODE HERE
+    >>> decrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> decrypt_vigenere("python", "a")
+    'python'
+    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
+    'ATTACKATDAWN'
+    """
     plaintext = ""
-    for i in ciphertext:
-        if 65 <= ord(i) <= 90:
-            if ord(i) - 3 < ord("A"):
-                plaintext += chr(ord(i) - 3 + 26)
+    for ch in ciphertext:
+        if "A" <= ch <= "Z":
+            if ch - 3 < "A":
+                plaintext += chr(ch - 3 + 26)
             else:
-                plaintext += chr(ord(i) - 3)
+                plaintext += chr(ch - 3)
         elif 97 <= ord(i) <= 122:
-            if ord(i) - 3 < ord("a"):
-                plaintext += chr(ord(i) - 3 + 26)
+            if ch - 3 < "a":
+                plaintext += chr(ch - 3 + 26)
             else:
-                plaintext += chr(ord(i) - 3)
+                plaintext += chr(ch - 3)
         else:
             plaintext += i
     return plaintext
